18b04956c36f0d548426934536b4f4b2ca597308
davidasix <davidandersonsix@gmail.com>
2022-07-27 15:52:41 -0400

    Converted Home to function based component
    Added imports for BLE

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,138 +1,167 @@
-import React from 'react';
-import {Text, View, TouchableOpacity, Image} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  NativeModules,
+  NativeEventEmitter,
+} from 'react-native';
+import {stringToBytes} from 'convert-string';
+import BleManager from 'react-native-ble-manager';
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 import SSIcon from 'react-native-vector-icons/SimpleLineIcons';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      r: false,
-      y: false,
-      g: false,
-    };
-  }
+const Home = props => {
+  const [r, setRed] = useState(false);
+  const [y, setYellow] = useState(false);
+  const [g, setGreen] = useState(false);
+  const [connectedDevices, setConnectedDevices] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState(false);
 
-  changeLight(l) {
-    this.setState({[l]: !this.state[l]});
-  }
+  useEffect(() => {
+    const bleSetup = async () => {
+      await BleManager.start({showAlert: true});
+      let connected = await BleManager.getConnectedPeripherals([]);
+      setConnectedDevices(connected);
+    };
+    bleSetup();
+  }, []);
+
+  const changeLight = async l => {
+    if (!selectedDevice) {
+      return console.log('Select a device');
+    }
+    try {
+      let text = stringToBytes('r');
+      let w = await BleManager.write(
+        selectedDevice.id,
+        '0000ffe0-0000-1000-8000-00805f9b34fb',
+        '0000ffe1-0000-1000-8000-00805f9b34fb',
+        text,
+      );
+      console.log({w});
+    } catch (e) {
+      console.log(e);
+    }
+    let funcs = {r: setRed, y: setYellow, g: setGreen};
+    funcs[l](![l]);
+  };
   /**
    * would be cool to see the stoplight have a little movement if you drag on the edge of it
    ***/
-  render() {
-    let {r, y, g} = this.state;
-    return (
-      <View style={styles.pageContainer}>
-        <View style={styles.headerContainer}>
-          <View style={{flex: 1}} />
-          <View
-            style={{flex: 3, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={styles.title}>
-              {false ? 'Connected' : 'Not Connected'}
-            </Text>
-          </View>
-          <View
-            style={{flex: 1, justifyContent: 'center', alignItems: 'flex-end'}}>
-            <SSIcon
-              name="settings"
-              size={35}
-              color="#FFF"
-              onPress={() => this.props.navigation.navigate('settings')}
-            />
-          </View>
+  return (
+    <View style={styles.pageContainer}>
+      <View style={styles.headerContainer}>
+        <View style={{flex: 1}} />
+        <View style={{flex: 3, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={styles.title}>
+            {false ? 'Connected' : 'Not Connected'}
+          </Text>
         </View>
+        <View
+          style={{flex: 1, justifyContent: 'center', alignItems: 'flex-end'}}>
+          <SSIcon
+            name="settings"
+            size={35}
+            color="#FFF"
+            onPress={() => props.navigation.navigate('settings')}
+          />
+        </View>
+      </View>
 
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <View style={styles.pole}>
-            <Image
-              style={{
-                width: '100%',
-                height: '100%',
-                position: 'absolute',
-                opacity: 0.5,
-              }}
-              source={require('../../assets/MetalTexture.jpg')}
-              resizeMode="cover"
-            />
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={styles.pole}>
+          <Image
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+              opacity: 0.5,
+            }}
+            source={require('../../assets/MetalTexture.jpg')}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.outline}>
+          <View style={styles.lightBox}>
+            <TouchableOpacity
+              onPress={() => changeLight('r')}
+              style={[
+                styles.lamp,
+                {backgroundColor: 'hsl(0, 10%, 50%)'},
+                r && [
+                  styles.lampOn,
+                  {backgroundColor: '#fc4444', shadowColor: '#FF0000'},
+                ],
+              ]}>
+              <Image
+                style={{
+                  width: '100%',
+                  position: 'absolute',
+                  height: '100%',
+                  opacity: 0.4,
+                }}
+                source={require('../../assets/glassTexture.png')}
+                resizeMode="cover"
+              />
+            </TouchableOpacity>
           </View>
-          <View style={styles.outline}>
-            <View style={styles.lightBox}>
-              <TouchableOpacity
-                onPress={() => this.changeLight('r')}
-                style={[
-                  styles.lamp,
-                  {backgroundColor: 'hsl(0, 10%, 50%)'},
-                  r && [
-                    styles.lampOn,
-                    {backgroundColor: '#fc4444', shadowColor: '#FF0000'},
-                  ],
-                ]}>
-                <Image
-                  style={{
-                    width: '100%',
-                    position: 'absolute',
-                    height: '100%',
-                    opacity: 0.4,
-                  }}
-                  source={require('../../assets/glassTexture.png')}
-                  resizeMode="cover"
-                />
-              </TouchableOpacity>
-            </View>
-            <View
-              style={[styles.lightBox, {width: `${(2 / 3) * (3 / 4) * 100}%`}]}>
-              <TouchableOpacity
-                onPress={() => this.changeLight('y')}
-                style={[
-                  styles.lamp,
-                  {backgroundColor: 'hsl(60, 10%, 50%)'},
-                  y && [
-                    styles.lampOn,
-                    {backgroundColor: '#f9fc44', shadowColor: '#FFFF00'},
-                  ],
-                ]}>
-                <Image
-                  style={{
-                    width: '100%',
-                    position: 'absolute',
-                    height: '100%',
-                    opacity: 0.4,
-                  }}
-                  source={require('../../assets/glassTexture.png')}
-                  resizeMode="cover"
-                />
-              </TouchableOpacity>
-            </View>
-            <View
-              style={[styles.lightBox, {width: `${(2 / 3) * (3 / 4) * 100}%`}]}>
-              <TouchableOpacity
-                onPress={() => this.changeLight('g')}
-                style={[
-                  styles.lamp,
-                  {backgroundColor: 'hsl(126, 10%, 50%)'},
-                  g && [
-                    styles.lampOn,
-                    {backgroundColor: '#44fc57', shadowColor: '#00FF00'},
-                  ],
-                ]}>
-                <Image
-                  style={{
-                    width: '100%',
-                    position: 'absolute',
-                    height: '100%',
-                    opacity: 0.4,
-                  }}
-                  source={require('../../assets/glassTexture.png')}
-                  resizeMode="cover"
-                />
-              </TouchableOpacity>
-            </View>
+          <View
+            style={[styles.lightBox, {width: `${(2 / 3) * (3 / 4) * 100}%`}]}>
+            <TouchableOpacity
+              onPress={() => changeLight('y')}
+              style={[
+                styles.lamp,
+                {backgroundColor: 'hsl(60, 10%, 50%)'},
+                y && [
+                  styles.lampOn,
+                  {backgroundColor: '#f9fc44', shadowColor: '#FFFF00'},
+                ],
+              ]}>
+              <Image
+                style={{
+                  width: '100%',
+                  position: 'absolute',
+                  height: '100%',
+                  opacity: 0.4,
+                }}
+                source={require('../../assets/glassTexture.png')}
+                resizeMode="cover"
+              />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={[styles.lightBox, {width: `${(2 / 3) * (3 / 4) * 100}%`}]}>
+            <TouchableOpacity
+              onPress={() => changeLight('g')}
+              style={[
+                styles.lamp,
+                {backgroundColor: 'hsl(126, 10%, 50%)'},
+                g && [
+                  styles.lampOn,
+                  {backgroundColor: '#44fc57', shadowColor: '#00FF00'},
+                ],
+              ]}>
+              <Image
+                style={{
+                  width: '100%',
+                  position: 'absolute',
+                  height: '100%',
+                  opacity: 0.4,
+                }}
+                source={require('../../assets/glassTexture.png')}
+                resizeMode="cover"
+              />
+            </TouchableOpacity>
           </View>
         </View>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 const styles = {
   pageContainer: {
